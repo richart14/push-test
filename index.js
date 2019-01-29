c24d8eeb9310c6947bb7cdcad1148925922f29b4
Richard Tom <tomrichard14@gmail.com>
2019-01-29 14:53:19 -0800

    add home screen

--- index.js
+++ index.js
@@ -35,29 +35,23 @@ console.log(Notification.permission)
 
 let deferredPrompt;
 
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-});
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  btnAdd.style.display = 'block';
-
-  btnAdd.addEventListener('click', (e) => {
+  addBtn.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'none';
+    addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
+    deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
           console.log('User accepted the A2HS prompt');
         } else {
@@ -67,4 +61,3 @@ window.addEventListener('beforeinstallprompt', (e) => {
       });
   });
 });
-
